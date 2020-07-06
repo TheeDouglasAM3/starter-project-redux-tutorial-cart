abc1ee446c48ffec54308c68ff100ad01b5dffad
Douglas Marcelino <douglas_champion@hotmail.com>
2020-07-06 18:24:11 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
+//VIDEO PAROU EM 51:15
 import React from "react";
 // components
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
+
 // redux stuff
+import {createStore} from 'redux'
+import { INCREASE, DECREASE } from './actions'
+import reducer from './reducer'
+
+//initial store 
+const initialStore = {
+  count: 0
+}
+
+const store = createStore(reducer, initialStore)
+store.dispatch({type: INCREASE})
+store.dispatch({type: DECREASE})
+store.dispatch({type: INCREASE})
+console.log(store.getState())
 
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar />
+      <Navbar cart={store.getState()} />
       <CartContainer cart={cartItems} />
     </main>
   );
