00385cb6073ee9e929ea57c60d62e0d1190b6b67
Douglas Marcelino <douglas_champion@hotmail.com>
2020-07-08 03:31:08 -0300

    Create action INCREASE

--- src/reducer.js
+++ src/reducer.js
@@ -14,7 +14,18 @@ function reducer(state, action) {
 
     if(action.type === INCREASE){
 
-        console.log("you increased amount")
+        let tempCart = state.cart.map((cartItem) => {
+            if(cartItem.id === action.payload.id){
+                cartItem = {...cartItem, amount: cartItem.amount + 1}
+                return cartItem
+            }
+            return cartItem
+        })
+        
+        return {
+            ...state, 
+            cart: tempCart
+        }
     }
 
     if(action.type === REMOVE){
